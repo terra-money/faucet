1306ad360adf5dc06b817115fa0e6dc93a42574d
CUI DI <cuidi.me@gmail.com>
2019-04-02 19:55:57 +0900

    1. fixed issues 	https://github.com/terra-project/faucet/issues/5 	https://github.com/terra-project/faucet/issues/5 2. add reaptcha 3. add axios post

--- frontend/src/Pages/About/About.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function AboutComponent() {
-  return <h1>About Component</h1>;
-}
-
-export default AboutComponent;
--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -1,90 +1,182 @@
-import React from 'react';
+import React from "react";
 // import cx from "classnames";
+import Reaptcha from "reaptcha";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import b32 from "../../scripts/b32";
 
-import '../../App.scss';
+import "react-toastify/dist/ReactToastify.css";
 
-function HomeComponent() {
-  return (
-    <div>
-      <section>
-        <h2>Terra Testnet Faucet</h2>
-        <article>
-          Hello intrepid spaceperson! Use this faucet to get tokens for the
-          latest Terra testnet. Please don't abuse this service—the number of
-          available tokens is limited.
-        </article>
-        <div className="recaptcha">
-          <div>
-            <div>
-              <iframe
-                title="recaptcha"
-                src="https://www.google.com/recaptcha/api2/anchor?ar=2&amp;k=6Ld4w4cUAAAAAJceMYGpOTpjiJtMS_xvzOg643ix&amp;co=aHR0cDovL2ZhdWNldC50ZXJyYS5tb25leTo4MA..&amp;hl=ko&amp;v=v1552285980763&amp;size=normal&amp;cb=oiiw6vnnpfvz"
-                width="304"
-                height="78"
-                role="presentation"
-                name="a-hl7t8lstwbxg"
-                frameborder="0"
-                scrolling="no"
-                sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation allow-modals allow-popups-to-escape-sandbox"
-              />
-            </div>
+import "../../App.scss";
+
+const bech32Validate = param => {
+  try {
+    b32.decode(param);
+  } catch (error) {
+    return error.message;
+  }
+};
+
+const sendSchema = Yup.object().shape({
+  address: Yup.string().required("Required"),
+  denom: Yup.string().required("Required")
+});
+
+class HomeComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sending: false,
+      verified: false
+    };
+  }
+
+  onVerify = () => {
+    this.setState({
+      verified: true
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnVisibilityChange
+          pauseOnHover
+        />
+        <section>
+          <h2>Terra Testnet Faucet</h2>
+          <article>
+            Hello intrepid spaceperson! Use this faucet to get tokens for the
+            latest Terra testnet. Please don't abuse this service—the number of
+            available tokens is limited.
+          </article>
+          <div className="recaptcha">
+            <Reaptcha
+              sitekey="6Ld4w4cUAAAAAJceMYGpOTpjiJtMS_xvzOg643ix"
+              onVerify={this.onVerify}
+            />
           </div>
-        </div>
-        <div className="inputContainer">
-          <div className="input">
-            <input type="text" placeholder="Testnet address" />
+          <Formik
+            initialValues={{
+              address: "",
+              denom: ""
+            }}
+            validationSchema={sendSchema}
+            onSubmit={(values, { resetForm }) => {
+              console.log(resetForm);
+              // same shape as initial values
+              this.setState({ sending: true });
+              axios
+                .post("/claim", {
+                  address: values.address,
+                  denom: values.denom
+                })
+                .then(() => {
+                  this.setState({ sending: false });
+                  toast.success(
+                    `Successfully Sent, Sent tokens to ${this.fields.address}`
+                  );
+
+                  resetForm();
+                })
+                .catch(err => {
+                  this.setState({ sending: false });
+                  toast.error(
+                    `Error Sending, An error occurred while trying to send: "${
+                      err.message
+                    }"`
+                  );
+                });
+            }}
+          >
+            {({ errors, touched }) => (
+              <Form className="inputContainer">
+                <div className="input">
+                  <Field name="address" validate={bech32Validate} />
+                  {errors.address && touched.address ? (
+                    <div className="fieldError">{errors.address}</div>
+                  ) : null}
+                </div>
+                <div className="select">
+                  <Field className="select" component="select" name="denom">
+                    <option
+                      value=""
+                      disabled="disabled"
+                      selected="selected"
+                      hidden="hidden"
+                    >
+                      Select denom to receive...
+                    </option>
+                    <option value="mluna">Luna</option>
+                    <option value="mkrw">KRW</option>
+                    <option value="musd">USD</option>
+                    <option value="msdr">SDR</option>
+                    <option value="mgbp">GBP</option>
+                    <option value="meur">EUR</option>
+                    <option value="mjpy">JPY</option>
+                    <option value="mcny">CNY</option>
+                  </Field>
+                  {errors.denom && touched.denom ? (
+                    <div className="fieldError selectFieldError">
+                      {errors.denom}
+                    </div>
+                  ) : null}
+                  <div className="selectAddon">
+                    <i className="material-icons">arrow_drop_down</i>
+                  </div>
+                </div>
+
+                <div className="buttonContainer">
+                  <button
+                    disabled={!this.state.verified}
+                    type="submit"
+                  >
+                    <i aria-hidden="true" className="material-icons">
+                      send
+                    </i>
+                    <span>Send me tokens</span>
+                  </button>
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </section>
+        <section>
+          <h2>Don't you have a testnet address?</h2>
+          <article>
+            There's two ways to get one. The first is by using Station, the
+            crypto wallet for Terra. If you know command-line-fu, you can also
+            generate an address with the Terra SDK.
+          </article>
+          <div className="buttonContainer">
+            <button className="light">
+              <i aria-hidden="true" className="material-icons">
+                supervisor_account
+              </i>
+              <span>
+                <a
+                  href="https://github.com/terra-project/core/blob/develop/docs/guide/join-network.md"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Join the latest testnet
+                </a>
+              </span>
+            </button>
           </div>
-          <div className="select">
-            <select>
-              <option
-                value=""
-                disabled="disabled"
-                selected="selected"
-                hidden="hidden"
-              >
-                Select denom to receive...
-              </option>
-              <option value="luna">Luna</option>
-              <option value="krw">KRW</option>
-              <option value="usd">USD</option>
-              <option value="sdr">SDR</option>
-              <option value="gbp">GBP</option>
-              <option value="eur">EUR</option>
-              <option value="jpy">JPY</option>
-              <option value="cny">CNY</option>
-            </select>
-            <div class="selectAddon">
-              <i class="material-icons">arrow_drop_down</i>
-            </div>
-          </div>
-        </div>
-        <div className="buttonContainer">
-          <button>
-            <i aria-hidden="true" class="material-icons">
-              send
-            </i>
-            <span>Send me tokens</span>
-          </button>
-        </div>
-      </section>
-      <section>
-        <h2>Don't you have a testnet address?</h2>
-        <article>
-          There's two ways to get one. The first is by using Station, the crypto
-          wallet for Terra. If you know command-line-fu, you can also generate
-          an address with the Terra SDK.
-        </article>
-        <div className="buttonContainer">
-          <button className="light">
-            <i aria-hidden="true" class="material-icons">
-              supervisor_account
-            </i>
-            <span>Join the latest testnet</span>
-          </button>
-        </div>
-      </section>
-    </div>
-  );
+        </section>
+      </div>
+    );
+  }
 }
 
 export default HomeComponent;
