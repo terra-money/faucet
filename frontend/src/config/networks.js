3a95b73fe2f94f0fd2029e59b14c5b1bf352d83b
etienne-napoleone <etienne@terra.money>
2020-10-13 14:54:07 +0900

    cherry: pick soju removal

--- frontend/src/config/networks.js
+++ frontend/src/config/networks.js
@@ -1,10 +1,6 @@
 export default [
   {
-    key: 'soju-0014',
-    lcd: 'https://soju-fcd.terra.dev',
-  },
-  {
-    key: 'tequila-0002',
+    key: 'tequila-0004',
     lcd: 'https://tequila-fcd.terra.dev',
   },
 ];
