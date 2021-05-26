9a632517c441cd7d14927d805fb7c7d71854ebe7
Paul Kim <paul@terra.money>
2021-05-26 22:17:22 +0900

    add bombay-0005

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,10 @@
 export const networks = [
   {
     key: 'tequila-0004',
-    lcd: 'https://tequila-fcd.terra.dev',
+    lcd: 'https://tequila-lcd.terra.dev',
+  },
+  {
+    key: 'bombay-0005',
+    lcd: 'http://3.34.120.243:1317',
   },
 ];
