1ea677847f4d1747a75645e8c3dc5ce30a225c11
Paul Kim <paul@terra.money>
2022-05-24 17:13:51 +0900

    edit faucet urls

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,10 +1,10 @@
 export const networks = [
   {
     chainId: 'pisco-1',
-    faucetUrl: 'https://pisco-faucet.terra.dev',
+    faucetUrl: 'https://pisco-faucet.terra.dev/claim',
   },
   {
     chainId: 'bombay-12',
-    faucetUrl: 'https://bombay-faucet.terra.dev',
+    faucetUrl: 'https://bombay-faucet.terra.dev/claim',
   },
 ];
