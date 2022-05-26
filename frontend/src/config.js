aa42ceadac862db7d0dd1254a5657e0a094cde89
Paul Kim <paul@terra.money>
2022-05-26 14:30:19 +0900

    remove bombay

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -3,8 +3,4 @@ export const networks = [
     chainId: 'pisco-1',
     faucetUrl: 'https://pisco-faucet.terra.dev/claim',
   },
-  {
-    chainId: 'bombay-12',
-    faucetUrl: 'https://bombay-faucet.terra.dev/claim',
-  },
 ];
