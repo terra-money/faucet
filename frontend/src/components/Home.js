793ae077000bc90e08d1d48bba7a072a8fc0aac7
Paul Kim <paul@terra.money>
2022-05-26 13:11:33 +0900

    update finder link

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -83,7 +83,7 @@ class HomeComponent extends React.Component {
         if (response.code) {
           toast.error(`Error: ${response.raw_log || `code: ${response.code}`}`);
         } else {
-          const url = `https://finder.extraterrestrial.money/testnet/tx/${response.txhash}`;
+          const url = `https://finder.terra.money/testnet/tx/${response.txhash}`;
           toast.success(
             <div>
               <p>
